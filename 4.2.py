8b702965e5798719a77aef0396068fb395444fbc
Arsal2000 <45012260+Arsal2000@users.noreply.github.com>
2018-11-17 03:43:58 +0500

    Add files via upload

---
+++ 4.2.py
@@ -0,0 +1,11 @@
+forecast='It will be a sunny day today'
+a='day'
+print(forecast)
+#counting command
+print(forecast.find(a))
+print(forecast.count(a))
+#replacing command
+print(forecast.replace('sunny','cloudy'))
+
+
+      
---
+++ 4.3.py
@@ -0,0 +1,5 @@
+last='Smith'
+first='John'
+middle='Paul'
+name=(last +"\t"+ first +"\t"+ middle)
+print(name)
